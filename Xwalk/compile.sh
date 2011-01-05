328344db0d607715440016b3cf86884d7e5800f8
akahrama <akahrama@4ce43508-9825-a210-ada9-253d433d0d10>
2011-01-05 14:23:11 +0000

    Added command to copy parameter text files to bin/mm/parameter directory.

--- Xwalk/compile.sh
+++ Xwalk/compile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# requires SUN java 1.6
+# Compiles Xwalk from source code.
+# Requires SUN java 1.6.
 if [ ! -d bin ]
 then
     mkdir bin
 fi
 
-SRC=$(find | grep ".java$")
-javac -d bin $SRC
+# compile source files and add binaries to bin/ directory
+javac -sourcepath src/ -d bin/ src/Xwalk.java
+
+# add parameter text files to bin/ directory
+mkdir -p bin/mm/parameter
+cp src/mm/parameter/* bin/mm/parameter
